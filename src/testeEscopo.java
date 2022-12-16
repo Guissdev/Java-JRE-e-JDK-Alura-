561245fe35e4c2e2ad5abb1bed82921cc57177b4
Guilherme Santos <guiss.devs@gmail.com>
2022-12-16 18:43:28 -0300

    escopo

---
+++ src/testeEscopo.java
@@ -0,0 +1,27 @@
+
+public class testeEscopo {
+	public static void main(String[] args) {
+		System.out.println("testando condicionais");
+		int idade = 20;
+		int quantidadePessoas = 3;
+
+		// boolean acompanhado = quantidadePessoas >= 2;
+
+		boolean acompanhado;
+
+		if (quantidadePessoas >= 2) {
+			acompanhado = true;
+		} else {
+			acompanhado = false;
+		}
+
+		System.out.println("valor de acompanhado = " + acompanhado);
+
+		if (idade >= 18 && acompanhado) {
+			System.out.println("Você tem mais de 18 anos");
+			System.out.println("Seja bem vindo");
+		} else {
+			System.out.println("Infelizment você não pode entrar");
+		}
+	}
+}
